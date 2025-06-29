809c7faeed71b3820765fe002be7851b4cd70269
rafiedel <rafie.asadel@ui.ac.id>
2025-06-29 16:31:17 +0700

    cd: setup redis

--- learning_chatbot/settings.py
+++ learning_chatbot/settings.py
@@ -197,8 +197,8 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 
 # Celery (Pararel Function Execution)
 
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_BROKER_URL = os.getenv("REDIS_URL", "")
+CELERY_RESULT_BACKEND = os.getenv("REDIS_URL", "")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
